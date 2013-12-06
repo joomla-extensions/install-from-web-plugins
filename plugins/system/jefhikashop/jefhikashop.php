67e209057fd6e9fb3c8928593a5b44ff36dc3833
emavro <emavro@yahoo.com>
2013-12-06 21:19:01 +0200

    Update jefhikashop.php

--- plugins/system/jefhikashop/jefhikashop.php
+++ plugins/system/jefhikashop/jefhikashop.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  System.jefcbsubs
+ * @subpackage  System.jefhikashop
  *
  * @copyright   Copyright (C) 2013 Efthimios Mavrogeorgiadis. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -10,19 +10,16 @@
 defined('_JEXEC') or die;
 
 /**
- * JEF integration to allow automatic installation of extensions upon payment of a plan with CBSubs
+ * JEF integration to allow automatic installation of extensions upon payment of a plan with HikaShop
  *
  * @package     Joomla.Plugin
- * @subpackage  System.jefcbsubs
+ * @subpackage  System.jefhikashop
  * @since       2.5
  */
-class PlgSystemJefcbsubs extends JPlugin
+class PlgSystemJefhikashop extends JPlugin
 {
 	public function onAfterInitialise()
 	{
-		// Make sure we're not in the backend, user is logged in,
-		// and $_GET['sessid'] has no value
-		// Note: $_GET['sessid'] is set when the user backend requests file to install
 		$app = JFactory::getApplication();
 		$sessid = $app->input->get('sessid', null);
 		if ( !$app->isSite() || !JFactory::getUser()->id || !is_null( $sessid ) )
@@ -30,30 +27,6 @@ class PlgSystemJefcbsubs extends JPlugin
 			return;
 		}
 
-		// Load needed CB files
-		global $_CB_framework;
-		include_once JPATH_ADMINISTRATOR . '/components/com_comprofiler/plugin.foundation.php';
-		include_once JPATH_ADMINISTRATOR . '/components/com_comprofiler/library/cb/cb.database.php';
-		include_once JPATH_ADMINISTRATOR . '/components/com_comprofiler/plugin.class.php';
-		include_once( $_CB_framework->getCfg( 'absolute_path' ) . '/components/com_comprofiler/plugin/user/plug_cbpaidsubscriptions/cbpaidsubscriptions.class.php');
-
-		// Populate $activePlans with user plans that are still active
-		$activePlans = array();
-		$paidUserExtension =& cbpaidUserExtension::getInstance( $_CB_framework->myId() );
-		$subs = $paidUserExtension->getActiveSubscriptions();
-		foreach ( array_keys( $subs ) as $k ) {
-			$plan = $subs[$k]->getPlan();
-			$plan_id = $plan->get( 'id');
-			$activePlans[$plan_id] = strtolower( $subs[$k]->get( 'status' ) ) == 'a' ? true : false;
-		}
-
-		// Do nothing if user has no active plans
-		if ( !count( $activePlans ) )
-		{
-			return;
-		}
-		
-		// Make sure the needed data is stored in $_SESSION
 		$keys = array( 'installat', 'installapp', 'timestamp' );
 		$session = JFactory::getSession();
 		foreach ( $keys as $key )
@@ -69,44 +42,66 @@ class PlgSystemJefcbsubs extends JPlugin
 			return;
 		}
 
-		// Get JEFReg plugin parameters
 		$plugin = JPluginHelper::getPlugin( 'system', 'jefreg' );
 		$params = new JRegistry( $plugin->params );
 		$files = $params->get( 'files', null );
 		$files = preg_replace('/\s*=\s*>\s*/', '=>', $files);
 		$files = preg_replace('/^\s*\**\s*/', '*', $files);
 		$files = preg_split( '/\s+/', $files );
-
-		// Match the $_POST value of the JED ID [installapp] with the extensions available on the server
-		// as listed in the 'files' parameter of the JEFReg plugin
-		$installfrom = '';
+		$url = '';
+		$productid = 0;
 		foreach ( $files as $f )
 		{
-			// Check only lines that start with *cbsubs: in the 'files' parameter of the JEFReg plugin
-			preg_match( '/^\*cbsubs:'.$sessionvals['installapp'].'=>(.+?)&cbsubsplan=(\d+).*?$/', trim( $f ), $matches );
-
-			// Make sure the user has access to the requested product
-			if ( array_key_exists( $matches[2], $activePlans ) && $activePlans[$matches[2]] )
+			if ( preg_match( '/^\*hikashop:'.$sessionvals['installapp'].'=>(.+?)[\?&]hikashopprod=(\d+).*?$/', trim( $f ), $matches ) )
 			{
-				$url = JRoute::_( $matches[1] . '&sessid=' . session_id() );
-				$installfrom = '&installfrom=' . base64_encode( $url );
+				$productid = $matches[2];
 				break;
 			}
 		}
-
-		// Do nothing if the product does not exist or the user has no access rights to it
-		if ( !$installfrom )
+		if ( !$productid )
 		{
 			return;
 		}
 
-		// Set $_SESSION values to NULL once we are ready to redirect to user backend
+		include_once(rtrim(JPATH_ADMINISTRATOR) . '/components/com_hikashop/helpers/helper.php');
+
+		$prodClass = hikashop_get('class.product');
+		$product = $prodClass->get($productid);
+
+		$config =& hikashop_config();
+		$order_status_for_download = '"'.str_replace(',', '","', $config->get('order_status_for_download','confirmed,shipped')).'"';
+		$download_time_limit = $config->get('download_time_limit',0);
+		$deadline = '';
+		if ($download_time_limit) {
+			$deadline = ' AND (a.order_created + '.$download_time_limit.') >= UNIX_TIMESTAMP(NOW())';
+		}
+
+		$db = JFactory::getDbo();
+		$query = $db->getQuery(true);
+		$query
+			->select(array('a.order_id', 'c.file_id', 'd.file_pos'))
+			->from($db->quoteName(hikashop_table('order'), 'a'))
+			->join('RIGHT', $db->quoteName(hikashop_table('order_product'), 'b') . ' ON (' . $db->quoteName('a.order_id') . ' = ' . $db->quoteName('b.order_id') . ') AND (' . $db->quoteName('b.product_id') . ' = ' . $productid . ')')
+			->join('RIGHT', $db->quoteName(hikashop_table('file'), 'c') . ' ON (' . $db->quoteName('c.file_ref_id') . ' = ' . $productid . ')')
+			->join('LEFT', $db->quoteName(hikashop_table('download'), 'd') . ' ON (' . $db->quoteName('a.order_id') . ' = ' . $db->quoteName('d.order_id') . ' AND ' . $db->quoteName('c.file_id') . ' = ' . $db->quoteName('d.file_id') . ')')
+			->where($db->quoteName('a.order_user_id') . ' = '.hikashop_loadUser().' AND ' . $db->quoteName('a.order_status') . ' IN ('.$order_status_for_download.')'.$deadline);
+		$db->setQuery($query);
+		$files = $db->loadObjectList();
+		if (!count($files)) {
+			return;
+		}
+
+		$file_pos = '';
+		if($files[0]->file_pos > 0) {
+			$file_pos = '&file_pos='.$file->file_pos;
+		}
+		
+		$installfrom = '&installfrom='.base64_encode(JRoute::_(JURI::base().'index.php?option='.HIKASHOP_COMPONENT.'&ctrl=order&task=download&file_id='.$files[0]->file_id.'&order_id='.$files[0]->order_id.$file_pos.'&sessid='.session_id()));
+
 		foreach ( $keys as $key )
 		{
 			$session->set( 'jefreg.' . $key, null );
 		}
-
-		// Redirect back to user backend with installation information
 		$app->redirect( $sessionvals['installat'].$installfrom );
 	}
 }
