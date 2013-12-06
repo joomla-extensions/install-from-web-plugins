459ca1932bae4ee40a2262f7e52e45a47d46ca6e
emavro <emavro@yahoo.com>
2013-12-06 21:21:01 +0200

    Update jefreg.php

--- plugins/system/jefreg.php
+++ plugins/system/jefreg.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  System.jefhikashop
+ * @subpackage  System.jefreg
  *
  * @copyright   Copyright (C) 2013 Efthimios Mavrogeorgiadis. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -10,98 +10,141 @@
 defined('_JEXEC') or die;
 
 /**
- * JEF integration to allow automatic installation of extensions upon payment of a plan with HikaShop
+ * Support for download after registration through Joomla! Extension Finder
  *
  * @package     Joomla.Plugin
- * @subpackage  System.jefhikashop
- * @since       2.5
+ * @subpackage  System.jefreg
+ * @since       1.5
  */
-class PlgSystemJefhikashop extends JPlugin
+class plgSystemJefreg extends JPlugin
 {
+	var $_jefreg = array();
+	var $_sessionvals = array();
+	
+	private function getJefreg()
+	{
+		if (!count($this->_jefreg))
+		{
+			$this->_jefreg = array(
+				'installat'	=>	trim(base64_decode($_REQUEST['installat'])),
+				'installapp'	=>	((int) $_REQUEST['installapp']),
+				'timestamp'	=>	time(),
+			);
+		}
+		return $this->_jefreg;
+	}
+	
+	private function getSessionValues()
+	{
+		if (!$this->_sessionvals)
+		{
+			$session = JFactory::getSession();
+			$jefreg = $this->getJefreg();
+			foreach ($jefreg as $key => $value)
+			{
+				$this->_sessionvals[$key] = $session->get('jefreg.' . $key, null);
+			}
+		}
+		return $this->_sessionvals;
+	}
+	
+	private function setSessionValues($null = false)
+	{
+		$session = JFactory::getSession();
+		$jefreg = $this->getJefreg();
+		foreach ($jefreg as $key => $value)
+		{
+			if ($null)
+			{
+				$value = null;
+			}
+			$session->set('jefreg.' . $key, $value);
+		}
+	}
+	
+	private function setSessionValuesNull()
+	{
+		$this->setSessionValues(true);
+	}
+	
+	private function isDataOK()
+	{
+		$jefreg = $this->getJefreg();
+		
+		//http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+		$regex = '_^(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))$_iuS';
+
+		return preg_match($regex, $jefreg['installat']) &&
+			$this->getInstallFrom($jefreg['installapp']);
+	}
+	
+	private function isSessionOK()
+	{
+		$jefreg = $this->getSessionValues();
+		return $jefreg['timestamp'] &&
+			time() < $jefreg['timestamp'] + 18 * 60 * 60 &&
+			$jefreg['installat'] &&
+			$jefreg['installapp'];
+	}
+	
+	private function getInstallFrom($appid)
+	{
+		$files = $this->params->get('files', null);
+		$files = preg_replace('/\s*=\s*>\s*/', '=>', $files);
+		$files = preg_split('/\s+/', $files);
+		$installfrom = '';
+		foreach ($files as $f)
+		{
+			if (preg_match('/^'.$appid.'=>(.+)/', trim($f), $matches))
+			{
+				$installfrom = '&installfrom='.base64_encode($matches[1]);
+			}
+		}
+		return $installfrom;
+	}
+	
 	public function onAfterInitialise()
 	{
 		$app = JFactory::getApplication();
-		$sessid = $app->input->get('sessid', null);
-		if ( !$app->isSite() || !JFactory::getUser()->id || !is_null( $sessid ) )
+		if ($this->isDataOK())
 		{
-			return;
-		}
-
-		$keys = array( 'installat', 'installapp', 'timestamp' );
-		$session = JFactory::getSession();
-		foreach ( $keys as $key )
-		{
-			$sessionvals[$key] = $session->get( 'jefreg.' . $key, null );
-		}
-		$sessionOK = $sessionvals['timestamp'] &&
-			time() < $sessionvals['timestamp'] + 18 * 60 * 60 &&
-			!is_null($sessionvals['installat']) &&
-			!is_null($sessionvals['installapp']);
-		if ( !$sessionOK )
-		{
-			return;
-		}
-
-		$plugin = JPluginHelper::getPlugin( 'system', 'jefreg' );
-		$params = new JRegistry( $plugin->params );
-		$files = $params->get( 'files', null );
-		$files = preg_replace('/\s*=\s*>\s*/', '=>', $files);
-		$files = preg_replace('/^\s*\**\s*/', '*', $files);
-		$files = preg_split( '/\s+/', $files );
-		$url = '';
-		$productid = 0;
-		foreach ( $files as $f )
-		{
-			if ( preg_match( '/^\*hikashop:'.$sessionvals['installapp'].'=>(.+?)[\?&]hikashopprod=(\d+).*?$/', trim( $f ), $matches ) )
+			$this->setSessionValues();
+			if(JFactory::getUser()->id && $this->isSessionOK())
 			{
-				$productid = $matches[2];
-				break;
+				$jefreg = $this->getSessionValues();
+				$this->setSessionValuesNull();
+				$installfrom = $this->getInstallFrom($jefreg['installapp']);
+				$app->redirect($jefreg['installat'].$installfrom);
+			}
+			$entry = $this->params->get('entry', null);
+			if ($entry)
+			{
+				$app->redirect($entry);
+			}
+			else
+			{
+				$app->redirect(JRoute::_('index.php?option=com_user&view=login'));
 			}
 		}
-		if ( !$productid )
+		else
 		{
-			return;
+			$jefreg = $this->getJefreg();
+			if ($jefreg['installat']) {
+				$installfrom = '&installfrom='.base64_encode('Extension could not be found.');
+				$app->redirect($jefreg['installat'].$installfrom);
+			}
 		}
-
-		include_once(rtrim(JPATH_ADMINISTRATOR) . '/components/com_hikashop/helpers/helper.php');
-
-		$prodClass = hikashop_get('class.product');
-		$product = $prodClass->get($productid);
-
-		$config =& hikashop_config();
-		$order_status_for_download = '"'.str_replace(',', '","', $config->get('order_status_for_download','confirmed,shipped')).'"';
-		$download_time_limit = $config->get('download_time_limit',0);
-		$deadline = '';
-		if ($download_time_limit) {
-			$deadline = ' AND (a.order_created + '.$download_time_limit.') >= UNIX_TIMESTAMP(NOW())';
-		}
-
-		$db = JFactory::getDbo();
-		$query = $db->getQuery(true);
-		$query
-			->select(array('a.order_id', 'c.file_id', 'd.file_pos'))
-			->from($db->quoteName(hikashop_table('order'), 'a'))
-			->join('RIGHT', $db->quoteName(hikashop_table('order_product'), 'b') . ' ON (' . $db->quoteName('a.order_id') . ' = ' . $db->quoteName('b.order_id') . ') AND (' . $db->quoteName('b.product_id') . ' = ' . $productid . ')')
-			->join('RIGHT', $db->quoteName(hikashop_table('file'), 'c') . ' ON (' . $db->quoteName('c.file_ref_id') . ' = ' . $productid . ')')
-			->join('LEFT', $db->quoteName(hikashop_table('download'), 'd') . ' ON (' . $db->quoteName('a.order_id') . ' = ' . $db->quoteName('d.order_id') . ' AND ' . $db->quoteName('c.file_id') . ' = ' . $db->quoteName('d.file_id') . ')')
-			->where($db->quoteName('a.order_user_id') . ' = '.hikashop_loadUser().' AND ' . $db->quoteName('a.order_status') . ' IN ('.$order_status_for_download.')'.$deadline);
-		$db->setQuery($query);
-		$files = $db->loadObjectList();
-		if (!count($files)) {
-			return;
-		}
-
-		$file_pos = '';
-		if($files[0]->file_pos > 0) {
-			$file_pos = '&file_pos='.$file->file_pos;
-		}
-		
-		$installfrom = '&installfrom='.base64_encode(JRoute::_(JURI::base().'index.php?option='.HIKASHOP_COMPONENT.'&ctrl=order&task=download&file_id='.$files[0]->file_id.'&order_id='.$files[0]->order_id.$file_pos.'&sessid='.session_id()));
-
-		foreach ( $keys as $key )
+	}
+	
+	public function onLoginUser()
+	{
+		if ($this->isSessionOK())
 		{
-			$session->set( 'jefreg.' . $key, null );
+			$jefreg = $this->getSessionValues();
+			$this->setSessionValuesNull();
+			$installfrom = $this->getInstallFrom($jefreg['installapp']);
+			$app = JFactory::getApplication();
+			$app->redirect($jefreg['installat'].$installfrom);
 		}
-		$app->redirect( $sessionvals['installat'].$installfrom );
 	}
 }
