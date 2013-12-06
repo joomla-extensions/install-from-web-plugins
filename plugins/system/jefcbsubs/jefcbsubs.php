04ff794f94c389e8fe9946dfbe34efba5fd6e601
emavro <emavro@yahoo.com>
2013-12-06 21:16:17 +0200

    Update jefcbsubs.php

--- plugins/system/jefcbsubs/jefcbsubs.php
+++ plugins/system/jefcbsubs/jefcbsubs.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Plugin
- * @subpackage  System.jefreg
+ * @subpackage  System.jefcbsubs
  *
  * @copyright   Copyright (C) 2013 Efthimios Mavrogeorgiadis. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -9,318 +9,104 @@
 
 defined('_JEXEC') or die;
 
-jimport( 'joomla.session.storage' );
-JFormHelper::loadRuleClass('url');
-
 /**
- * Support for JEF (Install from Web) download after registration/purchase functionality
+ * JEF integration to allow automatic installation of extensions upon payment of a plan with CBSubs
  *
  * @package     Joomla.Plugin
- * @subpackage  System.jefreg
+ * @subpackage  System.jefcbsubs
  * @since       2.5
  */
-class PlgSystemJefreg extends JPlugin
+class PlgSystemJefcbsubs extends JPlugin
 {
-	var $_jefreg = array();		// $_POST data
-	var $_sessionvals = array();	// $_SESSION data
-	
-	/**
-	 * Populate/Return $_jefreg with $_POST data
-	 *
-	 * @return  array  $_POST data
-	 *
-	 */
-	private function getJEFReg()
-	{
-		if (!count($this->_jefreg))
-		{
-			$app = JFactory::getApplication();
-			$this->_jefreg = array(
-				'installat'	=>	base64_decode($app->input->get('installat', null, 'base64')),
-				'installapp'	=>	$app->input->get('installapp', null, 'int'),
-				'commercial'	=>	0,
-				'timestamp'	=>	time(),
-			);
-		}
-		return $this->_jefreg;
-	}
-	
-	/**
-	 * Set the commercial flag ON
-	 *
-	 * @return  void
-	 *
-	 */
-	private function setCommercialOn()
-	{
-		$session = JFactory::getSession();
-		$session->set('jefreg.commercial', 1);
-		$this->_jefreg['commercial'] = 1;
-	}
-	
-	/**
-	 * Check the commercial flag
-	 *
-	 * @return  boolean
-	 *
-	 */
-	private function isCommercial()
-	{
-		$session = JFactory::getSession();
-		return $session->get('jefreg.commercial');
-	}
-	
-	/**
-	 * Populate/Return $_sessionvals with $_POST data as stored in $_SESSION
-	 *
-	 * @return  array  $_POST data as stored in $_SESSION
-	 *
-	 */
-	private function getSessionValues()
-	{
-		if (!$this->_sessionvals)
-		{
-			$session = JFactory::getSession();
-			$jefreg = $this->getJEFReg();
-			foreach ($jefreg as $key => $value)
-			{
-				$this->_sessionvals[$key] = $session->get('jefreg.' . $key, null);
-			}
-		}
-		return $this->_sessionvals;
-	}
-	
-	/**
-	 * Copy $_POST data to $_SESSION
-	 *
-	 * @param   boolean	$null	If set to TRUE, all jefreg $_SESSION values are set to NULL
-	 *
-	 * @return  void
-	 *
-	 */
-	private function setSessionValues($null = false)
-	{
-		$session = JFactory::getSession();
-		$jefreg = $this->getJEFReg();
-		foreach ($jefreg as $key => $value)
-		{
-			if ($null)
-			{
-				$value = null;
-			}
-			$session->set('jefreg.' . $key, $value);
-		}
-	}
-	
-	/**
-	 * Set $_SESSION values to NULL once we are ready to redirect to user backend
-	 *
-	 * @return  void
-	 *
-	 */
-	private function setSessionValuesNull()
-	{
-		$this->setSessionValues(true);
-	}
-	
-	/**
-	 * Check if $_POST data is what we expect it to be
-	 *
-	 * @return  boolean  true = OK, false = NOT OK
-	 *
-	 */
-	private function isDataOK()
-	{
-		$jefreg = $this->getJEFReg();
-		
-		// Use JFormRuleUrl to check if $_POST['installat'] is valid URL
-		$field = new SimpleXMLElement('<field></field>');
-		$rule = new JFormRuleUrl;
-		return $rule->test($field, $jefreg['installat']) &&
-			$this->getInstallFrom($jefreg['installapp']);
-	}
-	
-	/**
-	 * Check if $_SESSION contains the data we need
-	 *
-	 * @return  boolean  true = OK, false = NOT OK
-	 *
-	 */
-	private function isSessionOK()
-	{
-		$jefreg = $this->getSessionValues();
-		return $jefreg['timestamp'] &&
-			time() < $jefreg['timestamp'] + 18 * 60 * 60 &&
-			!is_null($jefreg['installat']) &&
-			!is_null($jefreg['installapp']) &&
-			!$jefreg['commercial'];
-	}
-	
-	/**
-	 * Get the URL the user backend should access to directly retrieve the installation/update file
-	 *
-	 * @param   integer	$appid	Extension JED ID
-	 *
-	 * @return  string	URL pointing to package, XML file, or direct download script
-	 *
-	 */
-	private function getInstallFrom($appid)
-	{
-		// Basic clean up of 'files' parameter as set in plugin options
-		$files = $this->params->get('files', null);
-		$files = preg_replace('/\s*=\s*>\s*/', '=>', $files);
-		$files = preg_replace('/^\s*\**\s*/', '*', $files);
-		$files = preg_split('/\s+/', $files);
-
-		// Match the $_POST value of the JED ID [$appid] with the extensions available on the server
-		// as listed in the 'files' parameter of the plugin
-		$installfrom = '';
-		foreach ($files as $f)
-		{
-			// Check for file that is available after registration
-			if (preg_match('/^'.$appid.'=>(.+)/', trim($f), $matches))
-			{
-				$installfrom = '&installfrom='.base64_encode($matches[1].'&sessid='.session_id());
-			}
-			// Check for file that is available after purchase
-			elseif (preg_match('/^\*.*:'.$appid.'=>(.+)/', trim($f), $matches))
-			{
-				// If found, set commercial flag ON
-				$this->setCommercialOn();
-				$installfrom = $matches[1];
-			}
-		}
-		return $installfrom;
-	}
-	
-	/**
-	 * Remove $_SESSION data from the database
-	 *
-	 * @return  void
-	 *
-	 */
-	public function onAfterRoute()
-	{
-		$app = JFactory::getApplication();
-		if ( !$app->isSite() )
-		{
-			return;
-		}
-		
-		// Remove $_SESSION data from the database only if $_GET['sessid'] has a value
-		$sessid = $app->input->get('sessid', null);
-		if (!is_null($sessid))
-		{
-			$sesstore = & JSessionStorage::getInstance('database');
-			$sesstore->destroy($sessid);
-		}
-	}
-	
-	/**
-	 * Entry point to handle original $_POST data
-	 *
-	 * @return  void
-	 *
-	 */
 	public function onAfterInitialise()
 	{
-		// Make sure we're not in the backend
-		$app = JFactory::getApplication();
-		if (!$app->isSite())
-		{
-			return;
-		}
-
-		// Retrieve $_SESSION data from the database if $_GET['sessid'] has a value		
-		// Note: $_GET['sessid'] is set when the user backend requests file to install
-		$sessid = $app->input->get('sessid', null);
-		if (!is_null($sessid))
-		{
-			$sesstore = & JSessionStorage::getInstance('database');
-			$sdata = $sesstore->read($sessid);
-			session_decode($sdata);
-			return;
-		}
-		
-		// Check if $_POST data is what we expect it to be
-		if ($this->isDataOK())
-		{
-			// Copy $_POST data to $_SESSION
-			$this->setSessionValues();
-
-			// If user is already logged in and our $_SESSION values are OK,
-			// redirect back to user backend with installation information
-			if(JFactory::getUser()->id && $this->isSessionOK())
-			{
-				$jefreg = $this->getSessionValues();
-				$this->setSessionValuesNull();
-				$installfrom = $this->getInstallFrom($jefreg['installapp']);
-				$app->redirect($jefreg['installat'].$installfrom);
-			}
-
-			// Get the optional 'entry' parameter from the plugin options
-			$entry = $this->params->get('entry', null);
-
-			// If the extension is commercial, redirect to the URL submitted by the developer on JED/JEF
-			if ($this->isCommercial())
-			{
-				$jefreg = $this->getSessionValues();
-				$installfrom = $this->getInstallFrom($jefreg['installapp']);
-				$app->redirect(JRoute::_($installfrom));
-			}
-			// If the 'entry' parameter has been set, redirect there
-			elseif ($entry)
-			{
-				$app->redirect(JRoute::_($entry));
-			}
-			// If the 'entry' parameter has not been set, redirect to default login page
-			else
-			{
-				$app->redirect(JRoute::_('index.php?option=com_users&view=login'));
-			}
-		}
-		else
-		{
-			// If $_POST data fails validation testing, redirect to user backend with error message
-			$jefreg = $this->getJEFReg();
-			if ($jefreg['installat']) {
-				$installfrom = '&installfrom='.base64_encode('Extension could not be found.');
-				$app->redirect($jefreg['installat'].$installfrom);
-			}
-		}
-	}
-	
-	/**
-	 * Redirect user after login
-	 *
-	 * @return  void
-	 *
-	 */
-	public function onUserAfterLogin()
-	{
-		// Make sure we're not in the backend, the requested extension is not commercial,
+		// Make sure we're not in the backend, user is logged in,
 		// and $_GET['sessid'] has no value
 		// Note: $_GET['sessid'] is set when the user backend requests file to install
 		$app = JFactory::getApplication();
 		$sessid = $app->input->get('sessid', null);
-		if (!$app->isSite() || $this->isCommercial() || !is_null($sessid))
+		if ( !$app->isSite() || !JFactory::getUser()->id || !is_null( $sessid ) )
 		{
 			return;
 		}
 
-		// Make sure user login data is stored in $_SESSION and database
-		$session = JFactory::getSession();
-		$session->set('user', JFactory::getUser());
-		$sesstore = JSessionStorage::getInstance('database');
-		$sesstore->write(session_id(), session_encode());
+		// Load needed CB files
+		global $_CB_framework;
+		include_once JPATH_ADMINISTRATOR . '/components/com_comprofiler/plugin.foundation.php';
+		include_once JPATH_ADMINISTRATOR . '/components/com_comprofiler/library/cb/cb.database.php';
+		include_once JPATH_ADMINISTRATOR . '/components/com_comprofiler/plugin.class.php';
+		include_once( $_CB_framework->getCfg( 'absolute_path' ) . '/components/com_comprofiler/plugin/user/plug_cbpaidsubscriptions/cbpaidsubscriptions.class.php');
 
-		// If $_SESSION values are OK, redirect back to user backend with installation information
-		if ($this->isSessionOK())
-		{
-			$jefreg = $this->getSessionValues();
-			$this->setSessionValuesNull();
-			$installfrom = $this->getInstallFrom($jefreg['installapp']);
-			$app->redirect($jefreg['installat'].$installfrom);
+		// Populate $activePlans with user plans that are still active
+		$activePlans = array();
+		$paidUserExtension =& cbpaidUserExtension::getInstance( $_CB_framework->myId() );
+		$subs = $paidUserExtension->getActiveSubscriptions();
+		foreach ( array_keys( $subs ) as $k ) {
+			$plan = $subs[$k]->getPlan();
+			$plan_id = $plan->get( 'id');
+			$activePlans[$plan_id] = strtolower( $subs[$k]->get( 'status' ) ) == 'a' ? true : false;
 		}
+
+		// Do nothing if user has no active plans
+		if ( !count( $activePlans ) )
+		{
+			return;
+		}
+		
+		// Make sure the needed data is stored in $_SESSION
+		$keys = array( 'installat', 'installapp', 'timestamp' );
+		$session = JFactory::getSession();
+		foreach ( $keys as $key )
+		{
+			$sessionvals[$key] = $session->get( 'jefreg.' . $key, null );
+		}
+		$sessionOK = $sessionvals['timestamp'] &&
+			time() < $sessionvals['timestamp'] + 18 * 60 * 60 &&
+			!is_null($sessionvals['installat']) &&
+			!is_null($sessionvals['installapp']);
+		if ( !$sessionOK )
+		{
+			return;
+		}
+
+		// Get JEFReg plugin parameters
+		$plugin = JPluginHelper::getPlugin( 'system', 'jefreg' );
+		$params = new JRegistry( $plugin->params );
+		$files = $params->get( 'files', null );
+		$files = preg_replace('/\s*=\s*>\s*/', '=>', $files);
+		$files = preg_replace('/^\s*\**\s*/', '*', $files);
+		$files = preg_split( '/\s+/', $files );
+
+		// Match the $_POST value of the JED ID [installapp] with the extensions available on the server
+		// as listed in the 'files' parameter of the JEFReg plugin
+		$installfrom = '';
+		foreach ( $files as $f )
+		{
+			// Check only lines that start with *cbsubs: in the 'files' parameter of the JEFReg plugin
+			preg_match( '/^\*cbsubs:'.$sessionvals['installapp'].'=>(.+?)&cbsubsplan=(\d+).*?$/', trim( $f ), $matches );
+
+			// Make sure the user has access to the requested product
+			if ( array_key_exists( $matches[2], $activePlans ) && $activePlans[$matches[2]] )
+			{
+				$url = JRoute::_( $matches[1] . '&sessid=' . session_id() );
+				$installfrom = '&installfrom=' . base64_encode( $url );
+				break;
+			}
+		}
+
+		// Do nothing if the product does not exist or the user has no access rights to it
+		if ( !$installfrom )
+		{
+			return;
+		}
+
+		// Set $_SESSION values to NULL once we are ready to redirect to user backend
+		foreach ( $keys as $key )
+		{
+			$session->set( 'jefreg.' . $key, null );
+		}
+
+		// Redirect back to user backend with installation information
+		$app->redirect( $sessionvals['installat'].$installfrom );
 	}
 }
